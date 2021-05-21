d2f8f3a7930bd927f61a9b25cf7f4e388e26804b
justinbarnard <jbarnard0031@gmail.com>
2021-05-20 20:06:09 -0600

    TestC

--- app/src/androidTest/java/com/ui/espresso/PersonListTest.kt
+++ app/src/androidTest/java/com/ui/espresso/PersonListTest.kt
@@ -1,6 +1,15 @@
 package com.ui.espresso
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.ui.espresso.bases.TestBase
+import com.ui.espresso.matchers.CustomMatchers
+import org.hamcrest.CoreMatchers
+import org.junit.Test
 
 /**
  *   WORK HERE AND CODE AS MUCH TESTING AS YOU WANT
@@ -10,5 +19,31 @@ import com.ui.espresso.bases.TestBase
  */
 class PersonListTest : TestBase() {
     // WORK HERE test-c
+    @Test
+    fun testIsFlaticonDisplayed() {
+        Espresso.onView(ViewMatchers.withId(R.id.person_list_adapter_button)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.person_list_footer)).check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun testClickOnPersonAtPosition() {
+        Espresso.onView(ViewMatchers.withId(R.id.person_list_adapter_button)).perform(ViewActions.click())
+        Espresso.onData(CoreMatchers.anything()).atPosition(1).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.person_name)).check(ViewAssertions.matches(ViewMatchers.withText(PERSON_NAME)))
+        Espresso.onView(ViewMatchers.withId(R.id.person_messages)).check(ViewAssertions.matches(ViewMatchers.withText(PERSON_MESSAGE)))
+    }
+
+    @Test
+    fun testClickOnPersonByName(){
+        Espresso.onView(ViewMatchers.withId(R.id.person_list_adapter_button)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(PERSON_NAME)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.person_name)).check(ViewAssertions.matches(ViewMatchers.withText(PERSON_NAME)))
+    }
+
+    companion object {
+        private const val PERSON_NAME = "Code Master"
+        private const val PERSON_MESSAGE = "[I am Kotlin Master, still learning Kotlin]"
+        private const val PERSON_AGE = 30
+    }
 
 }
